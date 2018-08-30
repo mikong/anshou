0db0c9d4a7c50db9adf52421809c186369d05131
mikong <michaelgalero@gmail.com>
2018-08-30 19:13:15 +0800

    Add termion, and display initial UI.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+extern crate termion;
+
+use termion::raw::IntoRawMode;
+use termion::event::Key;
+use termion::input::TermRead;
+use std::io::{Write, stdout, stdin};
+
+use termion::{color, clear};
+
+const FORM: &'static str = "╔══════════════════════════════════╗\n\r\
+                            ║                                  ║\n\r\
+                            ║ Algorithm: SHA1                  ║\n\r\
+                            ║ Length: 10                       ║\n\r\
+                            ║ Password: ∙∙∙∙∙∙                 ║\n\r\
+                            ║ Domain: github.com               ║\n\r\
+                            ║                                  ║\n\r\
+                            ╚══════════════════════════════════╝\n\r";
+
 fn main() {
-    println!("Password: 123456");
+    print!("{clear}{fg}{bg}",
+        clear = clear::All,
+        fg = color::Fg(color::LightWhite),
+        bg = color::Bg(color::Blue));
+
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    stdout.write(FORM.as_bytes()).unwrap();
+
+    for c in stdin().keys() {
+        match c.unwrap() {
+            Key::Char('q') => break,
+            _ => continue,
+        }
+    }
+
+    println!("{fg}{bg}",
+        fg = color::Fg(color::Reset),
+        bg = color::Bg(color::Reset));
 }
