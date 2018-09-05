9c48f0d0979ab48c58a754b9aa3c4af895304b1d
mikong <michaelgalero@gmail.com>
2018-09-05 17:00:35 +0800

    Add regex crate. Add helper to check for a valid scheme.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+extern crate regex;
+
+use regex::Regex;
+
 pub fn is_valid(password: &str) -> bool {
     match password.chars().nth(0) {
         Some(c) => {
@@ -7,6 +11,12 @@ pub fn is_valid(password: &str) -> bool {
         },
         None => false,
     }
+}
+
+pub fn has_scheme(s: &str) -> bool {
+    let re = Regex::new(r"^([a-zA-Z][a-zA-Z0-9+-.]*)://").unwrap();
+
+    re.is_match(s)
 }
 
 #[cfg(test)]
@@ -32,5 +42,23 @@ mod tests {
 
         let no_uppercase = "ri39yfvq7c";
         assert!(!is_valid(no_uppercase));
+    }
+
+    #[test]
+    fn check_scheme() {
+        let http = "http://www.example.com";
+        assert!(has_scheme(http));
+
+        let https = "https://example.com";
+        assert!(has_scheme(https));
+
+        let complex = "HtT+p-50.00://complex.com";
+        assert!(has_scheme(complex));
+
+        let number = "9ttp://example.com";
+        assert!(!has_scheme(number));
+
+        let none = "example.com";
+        assert!(!has_scheme(none));
     }
 }
