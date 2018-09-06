ec80421df1a7b0a7bf87cea693929aea6a7a45c8
mikong <michaelgalero@gmail.com>
2018-09-06 18:10:20 +0800

    Add helper to extract domain root from domain or URL.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
 extern crate regex;
+extern crate publicsuffix;
+extern crate url;
+
+#[macro_use]
+extern crate lazy_static;
 
 use regex::Regex;
+use publicsuffix::List;
+use url::Url;
+
+lazy_static! {
+    static ref SUFFIX_LIST: List = List::fetch().unwrap();
+}
 
 pub fn is_valid(password: &str) -> bool {
     match password.chars().nth(0) {
@@ -17,6 +28,31 @@ pub fn has_scheme(s: &str) -> bool {
     let re = Regex::new(r"^([a-zA-Z][a-zA-Z0-9+-.]*)://").unwrap();
 
     re.is_match(s)
+}
+
+pub fn get_domain_root(input: &str) -> Result<String, &'static str> {
+    let input = if has_scheme(input) {
+        let input_url = match Url::parse(input) {
+            Ok(url) => url,
+            Err(_) => return Err("can't parse URL"),
+        };
+        match input_url.host_str() {
+            Some(s) => s.to_string(),
+            None => return Err("can't parse URL"),
+        }
+    } else {
+        input.to_string()
+    };
+
+    let domain = match SUFFIX_LIST.parse_domain(&input) {
+        Ok(d) => d,
+        Err(_) => return Err("can't parse domain"),
+    };
+
+    match domain.root() {
+        Some(root) => Ok(root.to_string()),
+        None => Err("can't parse domain"),
+    }
 }
 
 #[cfg(test)]
@@ -60,5 +96,17 @@ mod tests {
 
         let none = "example.com";
         assert!(!has_scheme(none));
+    }
+
+    #[test]
+    fn extract_domain_root() {
+        let domain = "www.google.com";
+        assert_eq!(get_domain_root(domain).unwrap(), "google.com".to_string());
+
+        let country_coded = "www.amazon.co.jp";
+        assert_eq!(get_domain_root(country_coded).unwrap(), "amazon.co.jp".to_string());
+
+        let url = "http://www.google.com";
+        assert_eq!(get_domain_root(url).unwrap(), "google.com".to_string());
     }
 }
