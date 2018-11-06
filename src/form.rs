66adb61e79505bdaf625d3ef52a28ecb991c392a
mikong <4162+mikong@users.noreply.github.com>
2018-11-06 13:27:53 +0800

    Base Form width on max elem width.

--- src/form.rs
+++ src/form.rs
@@ -2,6 +2,7 @@ use std::io::Write;
 
 trait Element {
     fn label(&self) -> &str;
+    fn width(&self) -> u16;
 }
 
 struct Password {
@@ -21,6 +22,10 @@ impl Password {
 impl Element for Password {
     fn label(&self) -> &'static str {
         &"Password"
+    }
+
+    fn width(&self) -> u16 {
+        35
     }
 }
 
@@ -42,16 +47,25 @@ impl Element for Domain {
     fn label(&self) -> &'static str {
         &"Domain"
     }
+
+    fn width(&self) -> u16 {
+        35
+    }
 }
 
 pub struct Form {
     elems: Vec<Box<Element>>,
+    width: u16,
 }
 
 impl Form {
     pub fn new() -> Self {
+        let elems: Vec<Box<Element>> = vec![Password::boxed(), Domain::boxed()];
+        let width = Self::calculate_width(&elems);
+
         Form {
-            elems: vec![Password::boxed(), Domain::boxed()]
+            elems,
+            width,
         }
     }
 
@@ -63,22 +77,46 @@ impl Form {
         out.write(self.render_bottom_border().as_bytes()).unwrap();
     }
 
-    fn render_top_border(&self) -> &'static str {
-        "╔══════════════════════════════════╗\n\r"
+    fn render_top_border(&self) -> String {
+        let mut s = "╔".to_string();
+        for _ in 2..self.width {
+            s.push('═');
+        }
+        s.push_str("╗\n\r");
+        s
     }
 
-    fn render_empty_row(&self) -> &'static str {
-        "║                                  ║\n\r"
+    fn render_empty_row(&self) -> String {
+        let mut s = "║".to_string();
+        for _ in 2..self.width {
+            s.push(' ');
+        }
+        s.push_str("║\n\r");
+        s
     }
 
     fn render_elements(&self) -> &'static str {
-        "║ Algorithm: SHA1                  ║\n\r\
-         ║ Length: 10                       ║\n\r\
-         ║ Password: ∙∙∙∙∙∙                 ║\n\r\
-         ║ Domain: github.com               ║\n\r"
+        "║ Algorithm: SHA1                     ║\n\r\
+         ║ Length: 10                          ║\n\r\
+         ║ Password: ∙∙∙∙∙∙                    ║\n\r\
+         ║ Domain: github.com                  ║\n\r"
     }
 
-    fn render_bottom_border(&self) -> &'static str {
-        "╚══════════════════════════════════╝\n\r"
+    fn render_bottom_border(&self) -> String {
+        let mut s = "╚".to_string();
+        for _ in 2..self.width {
+            s.push('═');
+        }
+        s.push_str("╝\n\r");
+        s
+    }
+
+    fn calculate_width(elems: &Vec<Box<Element>>) -> u16 {
+        let border_width = 1;
+        let margin_width = 1;
+
+        let elem_width = elems.iter().map(|x| x.width()).max().unwrap_or(0);
+
+        2*border_width + 2*margin_width + elem_width
     }
 }
